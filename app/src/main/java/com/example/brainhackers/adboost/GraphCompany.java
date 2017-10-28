d85ab972a88f3bb21125ba596ca92872cb56dfff
Palak Jain <21954525+till-tomorrow@users.noreply.github.com>
2017-10-28 16:08:45 +0530

    Update GraphCompany.java

--- app/src/main/java/com/example/brainhackers/adboost/GraphCompany.java
+++ app/src/main/java/com/example/brainhackers/adboost/GraphCompany.java
@@ -1,20 +1,115 @@
-<?xml version="1.0" encoding="utf-8"?>
-<android.support.constraint.ConstraintLayout xmlns:android="http://schemas.android.com/apk/res/android"
-    android:layout_width="match_parent"
-    android:layout_height="match_parent"
-    xmlns:app="http://schemas.android.com/apk/res-auto">
-    <com.github.mikephil.charting.charts.BarChart
-        android:id="@+id/barChart"
-        android:layout_width="0dp"
-        android:layout_marginEnd="20dp"
-        app:layout_constraintRight_toRightOf="parent"
-        android:layout_marginRight="20dp"
-        android:layout_marginStart="20dp"
-        app:layout_constraintLeft_toLeftOf="parent"
-        android:layout_marginLeft="20dp"
-        android:layout_marginTop="20dp"
-        app:layout_constraintTop_toTopOf="parent"
-        android:layout_height="0dp"
-        app:layout_constraintBottom_toBottomOf="parent"
-        android:layout_marginBottom="20dp" />
-</android.support.constraint.ConstraintLayout>
+package com.example.shivang.facialrecognition;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.LargeValueFormatter;
+
+import java.util.ArrayList;
+
+
+public class GraphCompany extends AppCompatActivity {
+
+    private BarChart chart;
+    float barWidth;
+    float barSpace;
+    float groupSpace;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.graph_company);
+
+        barWidth = 0.3f;
+        barSpace = 0f;
+        groupSpace = 0.4f;
+
+        chart = (BarChart) findViewById(R.id.barChart);
+        chart.setDescription(null);
+        chart.setPinchZoom(false);
+        chart.setScaleEnabled(false);
+        chart.setDrawBarShadow(false);
+        chart.setDrawGridBackground(false);
+
+        int groupCount = 5;
+        ArrayList xVals = new ArrayList();
+        xVals.add("Co1");
+        xVals.add("Co2");
+        xVals.add("Co3");
+        xVals.add("Co4");
+        xVals.add("Co5");
+
+        ArrayList yVals1 = new ArrayList();
+        ArrayList yVals2 = new ArrayList();
+
+        yVals1.add(new BarEntry(1, (float)1));
+        yVals2.add(new BarEntry(1, (float)5));
+        yVals1.add(new BarEntry(2, (float)6));
+        yVals2.add(new BarEntry(2, (float)8));
+        yVals1.add(new BarEntry(3, (float)2));
+        yVals2.add(new BarEntry(3, (float)25));
+        yVals1.add(new BarEntry(4, (float)0));
+        yVals2.add(new BarEntry(4, (float)8));
+        yVals1.add(new BarEntry(5, (float)1));
+        yVals2.add(new BarEntry(5, (float)10));
+
+        BarDataSet set1, set2;
+        set1 = new BarDataSet(yVals1,"#People Called");
+        set1.setColor(Color.RED);
+        set2 = new BarDataSet(yVals2, "#People Reached");
+        set2.setColor(Color.BLUE);
+        BarData data = new BarData(set1, set2);
+        data.setValueFormatter(new LargeValueFormatter());
+        chart.setData(data);
+        chart.getBarData().setBarWidth(barWidth);
+        chart.getXAxis().setAxisMinimum(0);
+        chart.getXAxis().setAxisMaximum(0 + chart.getBarData().getGroupWidth(groupSpace, barSpace) * groupCount);
+        chart.groupBars(0, groupSpace, barSpace);
+
+        Legend l = chart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setDrawInside(true);
+        l.setYOffset(20f);
+        l.setXOffset(0f);
+        l.setYEntrySpace(0f);
+        l.setTextSize(8f);
+
+        //X-axis
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setGranularity(1f);
+        xAxis.setGranularityEnabled(true);
+        xAxis.setCenterAxisLabels(true);
+        xAxis.setDrawGridLines(false);
+        xAxis.setAxisMaximum(6);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(xVals));
+
+        //Y-axis
+        chart.getAxisRight().setEnabled(false);
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setValueFormatter(new LargeValueFormatter());
+        leftAxis.setDrawGridLines(true);
+        leftAxis.setSpaceTop(35f);
+        leftAxis.setAxisMinimum(0f);
+
+
+
+    }
+
+}
